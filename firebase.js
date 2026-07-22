ed19abe095a816dcd8359d97aa4d958847ac8b44
Tafajjul Ansari <tafajjulans141@gmail.com>
2026-07-22 23:11:14 +0530

    Fix Firebase database URL
    Updated the database URL in the Firebase configuration.

--- firebase.js
+++ firebase.js
@@ -1,19 +1,15 @@
-// ==================== FIREBASE SETUP ==================== //
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBZqEBkUo-XqtOC8U4Sf5U6YL1v93_P3Ew",
-  authDomain: "messtracker-01.firebaseapp.com",
-  databaseURL: "https://messtracker-01-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "messtracker-01",
-  storageBucket: "messtracker-01.firebasestorage.app",
-  messagingSenderId: "486545563057",
-  appId: "1:486545563057:web:e897b1768e39bea7c0f190",
-  measurementId: "G-C0JZ18F66R"
+    apiKey: "AIzaSyBZqEBkUo-XqtOC8U4Sf5U6YL1v93_P3Ew",
+    authDomain: "messtracker-01.firebaseapp.com",
+    projectId: "messtracker-01",
+    storageBucket: "messtracker-01.firebasestorage.app",
+    messagingSenderId: "486545563057",
+    appId: "1:486545563057:web:e897b1768e39bea7c0f190",
+    measurementId: "G-C0JZ18F66R",
+    databaseURL: "https://messtracker-01-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Database ko globally 'window.db' me save kar rahe hain taaki app.js ise use kar sake
 window.db = firebase.database();
